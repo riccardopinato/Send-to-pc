d0808aa88fa3b2d685293ead3b2fe6f28afeba13
riccardopinato <riccardo121296@gmail.com>
2026-09-21 16:59:39 +0200

    Bump app to v1.9.0 release candidate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.riccardopinato.inviaalpc"
         minSdk = 29
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.8.0"
+        versionCode = 20
+        versionName = "1.9.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
